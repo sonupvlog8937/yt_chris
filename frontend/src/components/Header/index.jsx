2e41d8e4f7eb5312021167c7c7708a06107fbfc9
sonupvlog8937 <sonupvlog8937@gmail.com>
2025-07-06 06:29:23 +0000

    hii

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <div className='top-strip'>
-                <div>header</div>
+            <div className='top-strip py-2'>
+                <div className='container'>
+                    <div className='flex item-center justify-between'>
+                        <div className='col1 w-[50%]'>
+                            <p>Get up to 50% off new season styles, limited time</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </header>
     )
